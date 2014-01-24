d58ff750888d2003309c4430e591e306b61a450a
Alexey Milogradov <alexey.milogradov@gmail.com>
2014-01-24 23:13:36 +0200

    Finishing the cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,4 +4,20 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+ 	fixtures :products
+  test "should add products to cart properly" do
+	  cart = Cart.create
+
+	  cart.add_product(products(:one).id).save!
+	  assert_equal cart.line_items.size, 1
+
+	  cart.add_product(products(:ruby).id).save!
+	  assert_equal cart.line_items.size, 2
+
+	  li = cart.add_product(products(:ruby).id)
+	  li.save!
+	  assert_equal li.quantity, 2, 'should increase line items quantity to 2'
+	  assert_equal cart.line_items.size, 2
+	end
 end
